662b051c76fe1132a014ef805bb8c1315c4bd2bf
Vinay Vishnu Jadhao <113461334+VinayJadhao@users.noreply.github.com>
2023-03-14 22:42:14 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -1,11 +1,9 @@
 import React from 'react'
 import '../styles/App.css';
+
 class App extends React.Component {
     constructor(){
         super();
-        this.name="John Doe"
-        this.age="34"
-        this.enrollmentNo="12345678"
         this.name="John Doe";
         this.state={
             age:34
@@ -22,8 +20,6 @@ class App extends React.Component {
     render(){
         return(
             <div>
-                <h1>Hello, my name is {this.name} </h1>
-                <p>I am {this.age} years old and my enrollment no is {this.enrollmentNo}</p>
                 <h1>Hello, my name is {this.name}</h1>
                 <p>I am {this.state.age} years old and my enrollment no is {this.enrollmentNo}</p>
                 <button onClick={()=>this.increase()}>increase</button>
@@ -31,4 +27,6 @@ class App extends React.Component {
         )
     }
 }
+
+
 export default App;
